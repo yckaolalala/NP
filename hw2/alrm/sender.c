04adfa5e3681e65167fcb865d32162344fd1e33e
YCKAO <yunchung0529@gmail.com>
2017-02-05 17:26:23 +0800

    udp file transmit

---
+++ hw2/alrm/sender.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+
+#define	SA	struct sockaddr
+#define	MAXLINE		1024	/* max text line length */
+#define MAXBUFF     6000
+#define LISTENQ 20
+
+int check[MAXBUFF];
+
+struct Header{
+    int fin; 
+    int index;
+    int total;
+    int size;
+};
+
+struct Frame{
+    struct Header ptr;
+    char buff[MAXLINE+16];
+};
+
+static void sig_alrm(int signo){
+        return;
+}
+
+int check_end(int total){
+    for(int i=0;i<total;i++)
+        if(check[i]==0)
+            return 0;
+    return 1;
+}
+
+void dg_cli(int sockfd, const SA *pservaddr, socklen_t servlen,char *name)
+{
+    int     n,nrecv;
+    char    sendline[MAXLINE], recvline[MAXLINE + 1];
+    FILE* fp=fopen(name,"r");
+    //char fin[]="fin";
+    char Fin[16];
+    struct Frame data[MAXBUFF];
+    struct Header fin;
+    struct Header ack;
+    fin.fin=1;
+    memcpy(Fin,&fin,sizeof(fin));
+
+    if(fp){
+        int file_fd=fileno(fp);
+        signal(SIGALRM,sig_alrm);
+        siginterrupt(SIGALRM,1);
+        int index=0;
+        while(1){//read file
+            memset(&sendline,0,MAXLINE);
+            n=read(file_fd,sendline,MAXLINE);
+            data[index].ptr.fin=0;
+            data[index].ptr.index=index;
+            data[index].ptr.total=0;
+            data[index].ptr.size=n;
+            memcpy(data[index].buff+sizeof(data[index].ptr),&sendline,n);
+            index++;
+            if(n==0)
+                break;
+        }
+
+        for(int i=0;i<index;i++){//add total 
+            data[i].ptr.total=index;
+            memcpy(data[i].buff,&data[i].ptr,sizeof(data[i].ptr));
+        }
+
+        while(!check_end(index)){
+            for(int i=0;i<index;i++){
+                if(check[i]!=1)
+                    write(sockfd,data[i].buff,data[i].ptr.size+sizeof(data[i].ptr));
+                if(index%1000==0)
+                    usleep(1);
+            }
+            while(1){
+                alarm(2);
+                if((nrecv= recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL))<0){
+                    if(errno==EINTR){
+                        break;
+                    }
+                }
+                else{
+                    alarm(0);
+                    memcpy(&ack,recvline,nrecv);
+                    check[ack.index]=1;
+                }
+            }
+
+        }
+
+        while(1){//send fin
+            sendto(sockfd,Fin,sizeof(Fin),0,pservaddr,servlen);
+            sendto(sockfd,Fin,sizeof(Fin),0,pservaddr,servlen);
+            sendto(sockfd,Fin,sizeof(Fin),0,pservaddr,servlen);
+            nrecv = recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL);
+            if(nrecv<0){
+                if(errno==EWOULDBLOCK){
+                    continue;
+                }
+            }
+            recvline[nrecv] = 0;    
+            break;
+        }
+
+        fclose(fp);
+    }
+
+}
+int main(int argc, char **argv)
+{
+    int     sockfd;
+    struct sockaddr_in servaddr;
+
+    if(argc != 4){
+        printf("usage: <IPaddress> <port> <filename>\n");
+        return 0;
+    }
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if(connect(sockfd,(SA *) &servaddr,sizeof(servaddr))==-1){
+        printf("Connect error\n");
+        exit(1);
+    }
+
+    for(int i=0;i<MAXBUFF;i++)
+        check[i]=0;
+    dg_cli(sockfd, (SA *) &servaddr, sizeof(servaddr),argv[3]);
+
+    return 0;
+}
---
+++ hw2/setsockopt/receiver.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+#include <signal.h>
+
+#define	SA	struct sockaddr
+#define	MAXLINE		1040	/* max text line length */
+#define	MAXBUFF		6000	
+#define LISTENQ 20
+
+int check[MAXBUFF];
+struct Header{
+    int fin;
+    int index;
+    int total;
+    int size;
+};
+struct Frame{
+    struct Header ptr;
+    char buff[MAXLINE];
+};
+
+int check_end(int total){
+    for(int i=0;i<total;i++)
+        if(check[i]==0)
+            return 0;
+    return 1;
+}
+
+void dg_write(int sockfd, SA *pcliaddr, socklen_t clilen,char *name)
+{
+    int     n;
+    socklen_t len;
+    char    mesg[MAXLINE];
+    char buff[MAXLINE];
+    char wbuff[MAXLINE];
+    char prebuff[MAXLINE];
+    char Ack[16];
+    FILE* fp=fopen(name,"w");
+    struct Frame data[MAXBUFF];
+    struct Header header,ack;
+
+    
+    int total;
+    if(fp){
+        int file_fd=fileno(fp);
+        while(1){
+            len = clilen;
+            memset(&buff,0,MAXLINE);
+            n = recvfrom(sockfd, buff, MAXLINE, 0, pcliaddr, &len);
+            memcpy(&header,buff,sizeof(header));
+            if(header.fin==0){
+                memcpy(&data[header.index].ptr,buff,sizeof(header));
+                memcpy(data[header.index].buff,buff+sizeof(header),data[header.index].ptr.size);
+                total=header.total;
+                memset(&ack,0,16);
+                ack.index=header.index;
+                memcpy(Ack,&ack,sizeof(ack));
+                sendto(sockfd, Ack,sizeof(Ack) , 0, pcliaddr, len);
+            }
+            if(header.fin==1 && check_end(total)){  
+                for(int i=0;i<total;i++){
+                   write(file_fd,data[i].buff,data[i].ptr.size);
+                }
+
+				struct timeval tv;
+    			tv.tv_sec = 0;
+    			tv.tv_usec = 200000;
+			    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+               while(1){//wait_close
+                    memset(&ack,0,16);
+                    ack.index=header.index;
+                    memcpy(Ack,&ack,sizeof(ack));
+                    sendto(sockfd, Ack,sizeof(Ack) , 0, pcliaddr, len);
+                    n = recvfrom(sockfd, buff, MAXLINE, 0, pcliaddr, &len);
+					if(n<0){
+						if(errno==EWOULDBLOCK){
+							exit(0);
+						}
+					}
+                }
+            }
+            else if(check[header.index]==0){
+                check[header.index]=1;
+            }
+        }
+        fclose(fp);
+    }  
+} 
+int main(int argc, char **argv)
+{
+    int     sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+
+    if(argc!=3){
+        printf("usage:<port> <filename>\n");
+        return 0;
+    }
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(sockfd, (SA *) &servaddr, sizeof(servaddr))==-1){
+        printf("bind fail\n");
+        exit(2);
+    }
+    
+    for(int i=0;i<MAXBUFF;i++){
+        check[i]=0;
+    }
+    dg_write(sockfd, (SA *) &cliaddr, sizeof(cliaddr),argv[2]);
+    close(sockfd);
+    return 0;
+}
+
+
